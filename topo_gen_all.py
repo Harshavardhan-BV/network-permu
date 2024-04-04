5bdf2e7019058c25f61db0d38cd77a516ba59c51
Harshavardhan-BV <bharshavardh@iisc.ac.in>
2024-04-04 17:12:40 +0530

    Feeling cute, might exploit symmetry

--- topo_gen_all.py
+++ topo_gen_all.py
@@ -20,13 +20,31 @@ G.add_nodes_from(range(0,n))
 
 # Pandas edgelist of complete graph
 df_c = nx.to_pandas_edgelist(nx.complete_graph(n,nx.DiGraph()))
-df_c['type']=2
 
 # Maximum number of edges (to permute)
-m_max = n*(n-1)
+m_max = (n*(n-1))//2
 
 # Make directories
 os.makedirs('./topofiles_'+str(n)+'/', exist_ok=True)
+
+def topo_conv(G1, m, g_n, d_type=2):
+    m = m if d_type==2 else n*(n-1)-m
+    # Convert the "unique" Graph to dataframe
+    df_g = nx.to_pandas_edgelist(G1)
+    # Start with the complete graph
+    df = df_c.copy()
+    df['type']=d_type
+    keys = ['source','target']
+    # Convert source, target to keys for complete graph
+    i1 = df[keys].set_index(keys).index
+    # Do the same for the unique graph
+    i2 = df_g.set_index(keys).index
+    # Set the edge as activation if edge exists in df_g
+    df.loc[i1.isin(i2),'type'] = 1 if d_type==2 else 2
+    # Rename nodes
+    df[keys] = df[keys].map(lambda x: chr(ord('A') + x))
+    # Save the topofile
+    df.to_csv('./topofiles_'+str(n)+'/'+str(m)+'_'+str(g_n)+'.topo',sep='\t',index=False)
 
 # Select the unique graphs with m edges network (ie do not repeat isomorphic ones) 
 def unique_graphs(m, save_topo=True):
@@ -50,21 +68,9 @@ def unique_graphs(m, save_topo=True):
             # or add it to unique list
             g_uniq.append(G1)
             if save_topo:
-                # Convert the "unique" Graph to dataframe
-                df_g = nx.to_pandas_edgelist(G1)
-                # Start with the complete graph
-                df = df_c.copy()            
-                keys = ['source','target']
-                # Convert source, target to keys for complete graph
-                i1 = df[keys].set_index(keys).index
-                # Do the same for the unique graph
-                i2 = df_g.set_index(keys).index
-                # Set the edge as activation if edge exists in df_g
-                df.loc[i1.isin(i2),'type']=1
-                # Rename nodes
-                df[keys] = df[keys].map(lambda x: chr(ord('A') + x))
-                # Save the topofile
-                df.to_csv('./topofiles_'+str(n)+'/'+str(m)+'_'+str(g_n)+'.topo',sep='\t',index=False)
+                topo_conv(G1, m, g_n)
+                if m!=m_max:
+                    topo_conv(G1, m, g_n, d_type=1)
             g_n+=1
     return g_uniq
 
@@ -75,5 +81,7 @@ with mp.Pool(ncores) as pool:
     bigg = pool.map(unique_graphs, range(m_max+1))
 # Get the length of elements in bigg
 bigg_len = [len(i) for i in bigg]
+# Exploiting the symmetry
+bigg_len = bigg_len + bigg_len[-2::-1]
 print(bigg_len, sum(bigg_len))
 
